891bcc2d3fa42f1cc323d77ec48bab3591464a05
munibk22 <Munib22@gmail.com>
2022-06-18 15:15:42 -0400

    test2

--- src/script.js
+++ src/script.js
@@ -44,3 +44,62 @@ const chart2 = new Chart(myChart2, {
         }
     }
 })
+
+let labels3 = ['Attack', 'Defense', 'Passing', 'Tackle', 'Speed'];
+let myChart3 = document.getElementById("myChart3").getContext('2d');
+
+let chart3 = new Chart(myChart3, {
+    type: 'radar',
+    data: {
+        labels: labels3,
+        datasets: [
+            {
+                label: 'Messi',
+                fill: true,
+                backgroundColor: "rgba(179, 181, 198, 0.2)",
+                borderColor: "rgba(179, 181, 198, 1)",
+                pointBorderColor: "#fff",
+                pointBackgroundColor: "rgba(179, 181, 198, 1)",
+                data: [50, 12, 55, 7, 29]
+            },
+            {
+                label: 'Ronaldo',
+                fill: true,
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor: "rgba(255, 99, 132, 1)",
+                pointBorderColor: "#fff",
+                pointBackgroundColor: "rgba(255, 99, 132, 1)",
+                data: [51, 10, 32, 20, 44]
+            }
+        ]
+    },
+    options: {
+        title: {
+            text: "Skills",
+            display: true
+        }
+    }
+});
+
+let labels4 = ['Germany', 'France', 'UK', 'Italy', 'Spain', 'Others(23)'];
+let data4 = [83, 67, 66, 61, 47, 187];
+let colors4 = ['#49A9EA', '#36CAAB', '#34495E', '#B370CF', '#AC5353', '#CFD4D8'];
+
+let myChart4 = document.getElementById("myChart4").getContext('2d');
+
+let chart4 = new Chart(myChart4, {
+    type: 'pie',
+    data: {
+        labels: labels4,
+        datasets: [{
+            data: data4,
+            backgroundColor: colors4
+        }]
+    },
+    options: {
+        title: {
+            text: "Population of the European Union (in mio)",
+            display: true
+        }
+    }
+});
